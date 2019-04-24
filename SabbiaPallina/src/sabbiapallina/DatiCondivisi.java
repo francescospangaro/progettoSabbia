730c5cf74f9ac33d845c7761bdf6e221a64ad0aa
franc_000 <franc_000@francescos>
2019-04-24 16:57:29 +0200

    daticondivisi conclusi e primoframe da finire

--- SabbiaPallina/src/sabbiapallina/DatiCondivisi.java
+++ SabbiaPallina/src/sabbiapallina/DatiCondivisi.java
@@ -10,5 +10,96 @@ package sabbiapallina;
  * @author Galimberti Francesco
  */
 public class DatiCondivisi {
+    private Sabbia[] sabbie;
+    private int running;
+    private int inclinazioneX;
+    private int inclinazioneY;
+    private float red, green, blue;
     
+    /* methods */
+
+    public DatiCondivisi() {
+    }
+
+    public DatiCondivisi(Sabbia[] sabbie, int running, int inclinazioneX, int inclinazioneY, float red, float green, float blue) {
+        this.sabbie = sabbie;
+        this.running = running;
+        this.inclinazioneX = inclinazioneX;
+        this.inclinazioneY = inclinazioneY;
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public Sabbia[] getSabbie() {
+        return sabbie;
+    }
+
+    public void setSabbie(Sabbia[] sabbie) {
+        this.sabbie = sabbie;
+    }
+
+    public int getRunning() {
+        return running;
+    }
+
+    public void setRunning(int running) {
+        this.running = running;
+    }
+
+    public int getInclinazioneX() {
+        return inclinazioneX;
+    }
+
+    public void setInclinazioneX(int inclinazioneX) {
+        this.inclinazioneX = inclinazioneX;
+    }
+
+    public int getInclinazioneY() {
+        return inclinazioneY;
+    }
+
+    public void setInclinazioneY(int inclinazioneY) {
+        this.inclinazioneY = inclinazioneY;
+    }
+
+    public float getRed() {
+        return red;
+    }
+
+    public void setRed(float red) {
+        this.red = red;
+    }
+
+    public float getGreen() {
+        return green;
+    }
+
+    public void setGreen(float green) {
+        this.green = green;
+    }
+
+    public float getBlue() {
+        return blue;
+    }
+
+    public void setBlue(float blue) {
+        this.blue = blue;
+    }
+    
+    public void incrementaInclX(){
+        this.inclinazioneX++;
+    }
+    
+    public void incrementaInclY(){
+        this.inclinazioneY++;
+    }
+    
+    public void diminuisciInclX(){
+        this.inclinazioneX--;
+    }
+    
+    public void diminuisciInclY(){
+        this.inclinazioneY--;
+    }
 }
