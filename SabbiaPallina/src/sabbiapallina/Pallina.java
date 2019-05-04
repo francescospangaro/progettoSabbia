65fb01fc4e6eeb87b0b0c22bbfd39b20c57a1268
franc <franc@DESKTOP-LBVMGHI>
2019-05-04 09:33:11 +0200

    modifiche

--- SabbiaPallina/src/sabbiapallina/Pallina.java
+++ SabbiaPallina/src/sabbiapallina/Pallina.java
@@ -10,5 +10,62 @@ package sabbiapallina;
  * @author Galimberti Francesco
  */
 public class Pallina {
+    private int rad;
+    private int velX;
+    private int velY;
+    private int dirX;
+    private int dirY;
+
+    public Pallina(int rad, int velX, int velY, int dirX, int dirY) {
+        this.rad = rad;
+        this.velX = velX;
+        this.velY = velY;
+        this.dirX = dirX;
+        this.dirY = dirY;
+    }
+
+    public Pallina() {
+    }
+
+    public int getRad() {
+        return rad;
+    }
+
+    public void setRad(int rad) {
+        this.rad = rad;
+    }
+
+    public int getVelX() {
+        return velX;
+    }
+
+    public void setVelX(int velX) {
+        this.velX = velX;
+    }
+
+    public int getVelY() {
+        return velY;
+    }
+
+    public void setVelY(int velY) {
+        this.velY = velY;
+    }
+
+    public int getDirX() {
+        return dirX;
+    }
+
+    public void setDirX(int dirX) {
+        this.dirX = dirX;
+    }
+
+    public int getDirY() {
+        return dirY;
+    }
+
+    public void setDirY(int dirY) {
+        this.dirY = dirY;
+    }
+    
     
 }
