b0a34efe7a163a83887f06780645ee7d20e8c04f
spangaro_francesco <spangaro_francesco@INF1-03.jeanmonnet.local>
2019-05-17 10:39:17 +0200

    Th Pallina

---
+++ SabbiaPallina/src/sabbiapallina/ThPallina.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sabbiapallina;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author spangaro_francesco
+ */
+public class ThPallina extends Thread {
+    DatiCondivisi datiC;
+    int idBall;
+
+}
